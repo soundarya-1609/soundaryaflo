8c3a871b4e27690034279fbddff37880fe579c40
soundarya-1609 <51950558+soundarya-1609@users.noreply.github.com>
2019-07-02 14:22:49 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,5 +1,3 @@
-chater=int(input())
-valu=list(map(int,input().split()[:chater]))
-valu.sort()
-for i in valu:
-  print(i,end=" ")
+sss=int(input())
+lid=list(map(int,input().split() [:sss]))
+print(min(lid))
